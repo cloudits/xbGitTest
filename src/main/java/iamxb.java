133b10bb63acb60a69925605ec2e1681f7cb6893
cloudchen <jchen@cloudits.info>
2017-02-18 20:19:26 +0800

    添加version方法，包含该方法的注释

--- src/main/java/iamxb.java
+++ src/main/java/iamxb.java
@@ -5,4 +5,8 @@ public class iamxb {
     public static void main(String[] args) {
         System.out.println("我是学霸君");
     }
+    public void version()
+    {
+        //感谢关注云层天咨微信公众号及腾讯课堂
+    }
 }
